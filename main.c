d172de72281e27f6f5c1ebedd17e6b98f47145cf
Thomas Borde <thomas.borde94@hotmail.fr>
2022-12-05 06:11:19 -0800

    Implemented fork() and exit

--- main.c
+++ main.c
@@ -9,14 +9,37 @@
 int main(void)
 {
 	size_t n;
-	char *buffer = NULL;
+	char *buffer = NULL, *exit = "exit";
 	char **argv;
 	char *fullcmd;
+	pid_t pid;
+	int keepgoing = 1;
 
-	getline(&buffer, &n, stdin);
-	argv = tokenizer(buffer);
-	fullcmd = malloc(sizeof(char) * 20);
-	fullcmd = find_path(argv[0]);
-	execve(fullcmd, argv, environ);
+
+	while (keepgoing)
+	{
+		getline(&buffer, &n, stdin);
+		argv = tokenizer(buffer);
+		if (argv == NULL)
+			continue;
+		if (_strcmp(argv[0], exit) == 0)
+			{
+				keepgoing = 0;
+				break;
+			}
+		fullcmd = malloc(sizeof(char) * 20);
+		fullcmd = find_path(argv[0]);
+		if (fullcmd != NULL)
+		{
+			pid = fork();
+			if (pid == 0)
+				execve(fullcmd, argv, environ);
+			else
+			{
+				wait(NULL);
+			}
+		}
+	}
+
 	return (0);
 }
